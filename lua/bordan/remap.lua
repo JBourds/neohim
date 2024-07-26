0454ae487eeb7acc6b2e8d0a491735cb8e5fa342
jbourdeau <jbourdeau@ndigital.com>
2024-07-26 16:55:34 -0400

    Cleaning up remaps I don't use, and adding some that I want.

--- lua/bordan/remap.lua
+++ lua/bordan/remap.lua
@@ -1,55 +1,47 @@
+-- Set map leader to be easily accessible
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
--- Very important
-vim.keymap.set("n", "<leader>l", function() open_linkedin() end, {noremap = true})
+-- Make going to the end of lines easier
+vim.keymap.set({"n", "v"}, "9", "$")
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+-- I just prefer this to keyboard motions (test these on Mac)
+vim.keymap.set({"n", "v"}, "<C-Left>", "B")
+vim.keymap.set({"n", "v"}, "<C-Right>", "E")
+
+-- Very important
+vim.keymap.set("n", "<leader>l", function() 
+    open_linkedin() 
+end, {noremap = true})
+
+-- Move highlighted region under/over next line
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 
 vim.keymap.set("n", "J", "mzJ`z")
+-- Quicker paging with cursor staying in place
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Some kind of pattern matching?
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
 -- Make terminal navigation easier
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 
--- greatest remap ever
-vim.keymap.set("x", "<leader>p", [["_dP]])
-
--- next greatest remap ever : asbjornHaland
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
-
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
-
--- This is going to get me cancelled
+-- Easier to leave insert mode
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
+-- What do these do?
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set(
-    "n",
-    "<leader>ee",
-    "oif err != nil {<CR>}<Esc>Oreturn err<Esc>"
-)
-
-vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
-vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
-
-vim.keymap.set("n", "<leader><leader>", function()
-    vim.cmd("so")
-end)
 
