a11cad5b45d4ed6cd2cc3aeb680940bdb8297f04
jbourdeau <jbourdeau@ndigital.com>
2024-07-26 16:28:00 -0400

    Some updates.

---
+++ after/plugin/urls.lua
@@ -0,0 +1,13 @@
+function open_url(url)
+  local OS = vim.loop.os_uname().sysname
+  if OS == "OSX32" or OS == "OSX64" then
+    os.execute('open "" "' .. url .. '"')
+  else
+    os.execute('start "" "' .. url .. '"')
+  end
+end
+
+function open_linkedin ()
+    open_url("https://linkedin.com")
+end
+
---
+++ lua/bordan/init.lua
@@ -0,0 +1,3 @@
+require('bordan.remap')
+require('bordan.set')
+vim.cmd('colorscheme kanagawa-wave')
