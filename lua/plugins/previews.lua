4181341a2afd68c98e281e51ea1f2496b05930c8
Jordan Bourdeau <jordanbourdeau03@gmail.com>
2026-01-23 10:58:25 -0500

    feat: Markdown preview plugin.

--- lua/plugins/previews.lua
+++ lua/plugins/previews.lua
@@ -1,20 +1,17 @@
 return {
-	{
-		"MeanderingProgrammer/render-markdown.nvim",
-		dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" },
-		---@module 'render-markdown'
-		---@type render.md.UserConfig
-		opts = {},
-		config = function()
-			require("render-markdown").setup({
-				completions = { lsp = { enabled = true } },
-			})
-		end,
-	},
-	{
-		"chomosuke/typst-preview.nvim",
-		lazy = false, -- or ft = 'typst'
-		version = "1.*",
-		opts = {}, -- lazy.nvim will implicitly calls `setup {}`
-	},
+    {
+        'brianhuster/live-preview.nvim',
+        dependencies = {
+            'nvim-telescope/telescope.nvim',
+        },
+        config = function()
+            require('livepreview.config').set()
+        end
+    },
+    {
+        "chomosuke/typst-preview.nvim",
+        lazy = false, -- or ft = 'typst'
+        version = "1.*",
+        opts = {},    -- lazy.nvim will implicitly calls `setup {}`
+    },
 }
