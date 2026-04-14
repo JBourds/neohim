30b2ab57afcfcd443d7331f48c6790d359fb0b39
Jordan Bourdeau <jordanbourdeau03@gmail.com>
2026-04-14 12:47:42 -0400

    chore: Updating treesitter and associated telescope version.

--- lua/plugins/previews.lua
+++ lua/plugins/previews.lua
@@ -10,8 +10,8 @@ return {
     },
     {
         "chomosuke/typst-preview.nvim",
-        lazy = false, -- or ft = 'typst'
+        lazy = true,
         version = "1.*",
-        opts = {},    -- lazy.nvim will implicitly calls `setup {}`
+        opts = {},
     },
 }
